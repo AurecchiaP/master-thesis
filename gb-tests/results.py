f1d4acb6755fcd38b715291c4791dbac3ad88815
AurecchiaP <paolo@aurecchia.ch>
2019-05-10 17:06:01 +0200

    started writing about tests

---
+++ gb-tests/results.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import os
+import sys
+import glob
+import pandas as pd
+import numpy as np
+import scipy.stats as st
+import matplotlib.pyplot as plt
+
+if len(sys.argv) < 4:
+    print ("Usage:", sys.argv[0], "<data dir> <pattern> <lat|tp>")
+    sys.exit(1)
+
+srcdir = str(sys.argv[1])
+pattern = str(sys.argv[2])
+op = str(sys.argv[3])
+
+if op != "lat" and op != "tp" and op != "skew":
+    print ("Invalid request:", op)
+    sys.exit(1)
+if op == "lat" or op == "tp":
+    allfiles = glob.glob(os.path.join(srcdir, pattern))
+    df_list = {}
+    for file in allfiles:
+        #print "Processing ", file
+        # tmp = pd.read_table(file, engine='python', header='infer', skipfooter=10)
+        tmp = pd.read_csv(file, sep='\t', engine='python', header='infer', skipfooter=10)
+        tmp['sec'] = tmp['ABS'] // 1000000
+        if op == "tp":
+            df_list[file] = tmp.groupby('sec').count()
+        elif op == "lat":
+            df_list[file] = tmp['LATENCY']
+
+if op == "tp":
+    panel = pd.Panel(df_list)
+    grouped = panel.sum(axis=0)
+    if len(sys.argv) > 4:
+        print(grouped['#ORDER'])
+        plt.plot(grouped)
+        plt.show()
+
+    discarded = grouped[5:len(grouped) - 5]  # sub the first and last 15 seconds
+    a = discarded['#ORDER']
+    (min, max) = st.t.interval(0.95, len(a) - 1, loc=np.mean(a), scale=st.sem(a))
+    print ('#', srcdir, np.mean(a), min, max)
+
+elif op == "skew":
+    lines = [int(line.rstrip('\n')) for line in open(srcdir)]
+    plt.plot(lines)
+    plt.show()
+elif op == "lat":
+    df = pd.concat(df_list)
+    #df = df[df.VALUES != 0]  # remove lines with no latencies
+    lat = df[int(len(df)*0.3):int(len(df)*0.9)]
+
+    # Choose how many bins you want here
+    data_set = sorted(set(lat))
+    num_bins = np.append(data_set, data_set[-1] + 1)
+
+    # Use the histogram function to bin the data
+    counts, bin_edges = np.histogram(lat, bins=num_bins)  # , normed=True)
+    counts = counts.astype(float) / len(lat)
+    # Now find the cdf
+    cdf = np.cumsum(counts)
+    cdf_plot = {'value': bin_edges[0:-1], 'percentage': cdf}
+    df = pd.DataFrame(cdf_plot)
+    pd.set_option('display.max_rows', len(df))
+    print ('#', df)
+    pd.reset_option('display.max_rows')
+    print ('#')
+    print ('#')
+    print ('#')
+    print ('#')
+    print ('#srcdir, mean, 5p, 25p, 50p, 75p, 95p, 99p')
+    print ('#',  srcdir, lat.mean(), np.percentile(lat, 5), np.percentile(lat, 25), np.percentile(lat, 50), np.percentile(lat, 75), np.percentile(lat, 95), np.percentile(lat, 99))
+
+    # And finally plot the cdf
+    if len(sys.argv) > 4:
+        plt.plot(bin_edges[0:-1], cdf)
+        plt.ylim((0, 1))
+        plt.show()
